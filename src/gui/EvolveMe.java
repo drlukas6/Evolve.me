5344817ce155d1fd057af16342b4bd29d1b396ec
drlukas6 <lsestic@gmail.com>
2018-12-28 00:58:35 +0100

    Fitness progress chart works

--- src/gui/EvolveMe.java
+++ src/gui/EvolveMe.java
@@ -5,14 +5,19 @@ import gui.networkInfo.NetworkInfo;
 import gui.panes.*;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.Map;
 
 public class EvolveMe extends Application {
 
@@ -60,7 +65,6 @@ public class EvolveMe extends Application {
             if (networkInputInfo == null || networkOutputInfo == null)
                 throw new IllegalStateException();
             try {
-
                 int rows = centerPane.getNumberOfRows();
                 int columns = centerPane.getNumberOfColumns();
                 int levelsBack = centerPane.getLevelsBack();
@@ -77,33 +81,28 @@ public class EvolveMe extends Application {
                         levelsBack, networkInputInfo.getDimension(), networkOutputInfo.getDimension(),
                         networkInputInfo.getValues(), networkOutputInfo.getValues().get(0), maxGenerations,
                         writer, centerPane.getCheckedOperations());
-                organism.performInitialExecution();
-                while (organism.getGeneration() < organism.getMaxGeneration()) {
-                    organism.completeGeneration();
-                    Thread.sleep(1000);
-                    bottomPane.updateProgressBy(1./organism.getMaxGeneration());
-                }
-//                new Thread(() -> {
-//
-//                    try {
-//                        Writer writer = new BufferedWriter(
-//                                new OutputStreamWriter(
-//                                        new FileOutputStream(String.format("Organisms/organism-%s.cgp", organismId)), StandardCharsets.UTF_8)
-//                        );
-//                        Organism organism = new Organism(numberOfNetworks, rows, columns,
-//                                levelsBack, networkInputInfo.getDimension(), networkOutputInfo.getDimension(),
-//                                networkInputInfo.getValues(), networkOutputInfo.getValues().get(0), maxGenerations,
-//                                writer, centerPane.getCheckedOperations());
-//                        organism.performInitialExecution();
-//                        while (organism.getGeneration() < organism.getMaxGeneration()) {
-//                            organism.completeGeneration();
-//                            Platform.runLater(() -> bottomPane.updateProgressBy(1./organism.getMaxGeneration()));
-//                        }
-//                    } catch (FileNotFoundException e1) {
-//                        e1.printStackTrace();
-//                    }
-//                }).run();
-
+                Service<Void> service = new Service<Void>() {
+                    @Override
+                    protected Task<Void> createTask() {
+                        return new Task<Void>() {
+                            @Override
+                            protected Void call() throws InterruptedException {
+                                organism.performInitialExecution();
+                                while (organism.getGeneration() < organism.getMaxGeneration()) {
+                                    organism.completeGeneration();
+                                    Platform.runLater(() -> {
+                                        bottomPane.updateProgressBy(1./organism.getMaxGeneration());
+                                    });
+                                }
+                                Platform.runLater(() -> {
+                                    showGraph(organism.getFitnessProgressData());
+                                });
+                                return null;
+                            }
+                        };
+                    }
+                };
+                service.start();
 
             } catch (NumberFormatException exception ) {
                 this.centerPane.setStatusText("Number Formatting error");
@@ -111,10 +110,36 @@ public class EvolveMe extends Application {
                 this.centerPane.setStatusText("File not found error");
             } catch (IllegalStateException exception) {
                 this.centerPane.setStatusText("Network info error");
-            } catch (InterruptedException e1) {
+            } /*catch (InterruptedException e1) {
                 e1.printStackTrace();
-            }
+            }*/
         });
+    }
+
+    private void showGraph(Map<Integer, Double> values) {
+        Stage graphWindow = new Stage();
+
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        LineChart<Number, Number> fitnessProgressGraph = new LineChart<>(xAxis, yAxis);
+        XYChart.Series series = new XYChart.Series();
+
+        xAxis.setLabel("Generation");
+        yAxis.setLabel("Fitness");
+        fitnessProgressGraph.setTitle("Fitness progress graph");
+        series.setName("Fitnes progress data");
+
+        for(Integer key: values.keySet()) {
+            Double value = values.get(key);
+            series.getData().add(new XYChart.Data<>(key, value));
+        }
+
+        Scene graphScene = new Scene(fitnessProgressGraph, 400, 400);
+        fitnessProgressGraph.getData().add(series);
+
+        graphWindow.setTitle("Fitness progress graph");
+        graphWindow.setScene(graphScene);
+        graphWindow.show();
     }
 
 }
